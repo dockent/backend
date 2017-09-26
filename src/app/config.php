424baad4e02887e35bc18c4b79f2ecbbb0fa50dc
Vladyslav Pozdnyakov <vladyslav.pozdnyakov@comodo.od.ua>
2017-09-26 17:00:18 +0300

    Default connection for queue

--- src/app/config.php
+++ src/app/config.php
@@ -11,7 +11,7 @@ return [
         'remote_socket' => 'localhost'
     ],
     'queue' => [
-        'host' => '',
-        'port' => ''
+        'host' => '127.0.0.1',
+        'port' => '11300'
     ]
 ];
