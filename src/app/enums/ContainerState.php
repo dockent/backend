39c36ab3da84333305352d83cc997facd6392f4a
Vladyslav Pozdnyakov <vladyslav.pozdnyakov@comodo.od.ua>
2017-11-01 18:50:16 +0200

    Some refactoring and fixes in views

---
+++ src/app/enums/ContainerState.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vpozdnyakov
+ * Date: 31.10.17
+ * Time: 17:02
+ */
+
+namespace Dockent\enums;
+
+/**
+ * Class ContainerState
+ * @package Dockent\enums
+ */
+final class ContainerState
+{
+    const RUNNING = 'running';
+}
