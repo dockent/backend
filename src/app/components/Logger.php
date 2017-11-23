3d1322efa6bff4a739b11f5ad43b19d1d3f7923d
Vladyslav Pozdnyakov <vladyslav.pozdnyakov@comodo.od.ua>
2017-11-23 17:19:24 +0200

    Make skeleton for logger component (adapter for logstash)

---
+++ src/app/components/Logger.php
@@ -0,0 +1,277 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vpozdnyakov
+ * Date: 23.11.17
+ * Time: 11:40
+ */
+
+namespace Dockent\components;
+
+use Phalcon\Logger as PhalconLogger;
+use Phalcon\Logger\AdapterInterface;
+use Phalcon\Logger\FormatterInterface;
+
+/**
+ * Class Logger
+ * @package Dockent\components
+ */
+class Logger implements AdapterInterface
+{
+    /**
+     * @var FormatterInterface
+     */
+    private $formatter;
+
+    /**
+     * @var int
+     */
+    private $logLevel = PhalconLogger::ERROR;
+
+    /**
+     * @var bool
+     */
+    private $transactionStatus = false;
+
+    /**
+     * @var array
+     */
+    private $transactionStack = [];
+
+    /**
+     * @var string
+     */
+    private $host;
+
+    /**
+     * @var int
+     */
+    private $port;
+
+    /**
+     * @var resource
+     */
+    private $socket = null;
+
+    /**
+     * Logger constructor.
+     * @param string $host
+     * @param int $port
+     */
+    public function __construct(string $host, int $port)
+    {
+        $this->host = $host;
+        $this->port = $port;
+    }
+
+    private function socketInitialize()
+    {
+
+    }
+
+    /**
+     * Sets the message formatter
+     *
+     * @param FormatterInterface $formatter
+     * @return AdapterInterface
+     */
+    public function setFormatter(FormatterInterface $formatter): AdapterInterface
+    {
+        $this->formatter = $formatter;
+        return $this;
+    }
+
+    /**
+     * Returns the internal formatter
+     *
+     * @return FormatterInterface
+     */
+    public function getFormatter(): FormatterInterface
+    {
+        return $this->formatter;
+    }
+
+    /**
+     * Filters the logs sent to the handlers to be greater or equals than a specific level
+     *
+     * @param int $level
+     * @return AdapterInterface
+     */
+    public function setLogLevel($level): AdapterInterface
+    {
+        $this->logLevel = $level;
+        return $this;
+    }
+
+    /**
+     * Returns the current log level
+     *
+     * @return int
+     */
+    public function getLogLevel(): int
+    {
+        return $this->logLevel;
+    }
+
+    /**
+     * Sends/Writes messages to the file log
+     *
+     * @param mixed $type
+     * @param mixed $message
+     * @param array $context
+     * @return AdapterInterface
+     */
+    public function log($type, $message = null, array $context = null): AdapterInterface
+    {
+        if ($this->transactionStatus) {
+            $this->transactionStack[] = [
+                'type' => $type,
+                'message' => $message,
+                'context' => $context
+            ];
+        }
+        if ($this->socket === null) {
+            $this->socketInitialize();
+        }
+        return $this;
+    }
+
+    /**
+     * Starts a transaction
+     *
+     * @return AdapterInterface
+     */
+    public function begin(): AdapterInterface
+    {
+        $this->transactionStatus = true;
+        return $this;
+    }
+
+    /**
+     * Commits the internal transaction
+     *
+     * @return AdapterInterface
+     */
+    public function commit(): AdapterInterface
+    {
+        $this->transactionStatus = false;
+        foreach ($this->transactionStack as $item) {
+            $this->log(...$item);
+        }
+        $this->transactionStack = [];
+        return $this;
+    }
+
+    /**
+     * Rollbacks the internal transaction
+     *
+     * @return AdapterInterface
+     */
+    public function rollback(): AdapterInterface
+    {
+        $this->transactionStack = [];
+        $this->transactionStatus = false;
+        return $this;
+    }
+
+    /**
+     * Closes the logger
+     *
+     * @return bool
+     */
+    public function close(): bool
+    {
+        // TODO: Implement close() method.
+    }
+
+    /**
+     * Sends/Writes a debug message to the log
+     *
+     * @param string $message
+     * @param array $context
+     * @return AdapterInterface
+     */
+    public function debug($message, array $context = null): AdapterInterface
+    {
+        $this->log(PhalconLogger::DEBUG, $message, $context);
+        return $this;
+    }
+
+    /**
+     * Sends/Writes an error message to the log
+     *
+     * @param string $message
+     * @param array $context
+     * @return AdapterInterface
+     */
+    public function error($message, array $context = null): AdapterInterface
+    {
+        $this->log(PhalconLogger::ERROR, $message, $context);
+        return $this;
+    }
+
+    /**
+     * Sends/Writes an info message to the log
+     *
+     * @param string $message
+     * @param array $context
+     * @return AdapterInterface
+     */
+    public function info($message, array $context = null): AdapterInterface
+    {
+        $this->log(PhalconLogger::INFO, $message, $context);
+        return $this;
+    }
+
+    /**
+     * Sends/Writes a notice message to the log
+     *
+     * @param string $message
+     * @param array $context
+     * @return AdapterInterface
+     */
+    public function notice($message, array $context = null): AdapterInterface
+    {
+        $this->log(PhalconLogger::NOTICE, $message, $context);
+        return $this;
+    }
+
+    /**
+     * Sends/Writes a warning message to the log
+     *
+     * @param string $message
+     * @param array $context
+     * @return AdapterInterface
+     */
+    public function warning($message, array $context = null): AdapterInterface
+    {
+        $this->log(PhalconLogger::WARNING, $message, $context);
+        return $this;
+    }
+
+    /**
+     * Sends/Writes an alert message to the log
+     *
+     * @param string $message
+     * @param array $context
+     * @return AdapterInterface
+     */
+    public function alert($message, array $context = null): AdapterInterface
+    {
+        $this->log(PhalconLogger::ALERT, $message, $context);
+        return $this;
+    }
+
+    /**
+     * Sends/Writes an emergency message to the log
+     *
+     * @param string $message
+     * @param array $context
+     * @return AdapterInterface
+     */
+    public function emergency($message, array $context = null): AdapterInterface
+    {
+        $this->log(PhalconLogger::EMERGENCY, $message, $context);
+        return $this;
+    }
+}
