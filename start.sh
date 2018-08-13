2f3475bdb070cfe0014df98f4ea6f85d377c2108
Vladyslav Pozdnyakov <scary_donetskiy@live.com>
2018-08-13 14:13:36 +0300

    Set other port (according to frontend requests) for php application

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 beanstalkd -b /var/beanstore &
 php src/queue.php &
-php -S 0.0.0.0:80 -t $(pwd)/src $(pwd)/src/.htrouter.php
+php -S 0.0.0.0:8080 -t $(pwd)/src $(pwd)/src/.htrouter.php
